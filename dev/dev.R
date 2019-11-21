92013be1ef927f1fecec6a790a652fedd3d46143
gilles-guillot <gilles.b.guillot@gmail.com>
2019-11-21 15:27:33 +0100

    First commit

--- dev/dev.R
+++ dev/dev.R
@@ -1,6 +1,10 @@
 ## Some R commandes to help building the package
 library(devtools)
 library(roxygen2)
+library(usethis)
+use_git()
+use_github(auth_token = "37e8f5fd3b4bf0f1a407a694602c1f47b2122b43")
+
 setwd("/home/gilles/Dropbox/iPRI/projects/ToxPharm/MDR_package/")
 getwd()
 create('MDR')
