4d37948cb9180ea60a7727bcb93408bd39074612
gilles-guillot <gilles.b.guillot@gmail.com>
2020-01-14 15:32:15 +0100

    D. curvirostris included

---
+++ R/D.curvirostris.mix.R
@@ -0,0 +1,4 @@
+#' Data extracted from Dallabonna et al. Chemosphere (2014)
+#' @format an R object
+#' @source subset of numerical values from Table 1 and 2 in Dallabonna et al. Chemosphere (2014)
+"D.curvirostris.mix"
